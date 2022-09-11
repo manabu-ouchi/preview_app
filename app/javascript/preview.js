227c4bb06a200c05b2f9e8c4ed5f7521b06796d2
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-09-11 20:49:11 +0900

    生成した要素をブラウザに表示

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -20,5 +20,9 @@ document.addEventListener('DOMContentLoaded', function(){
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src', blob);
+
+    // 生成したHTMLの要素をブラウザに表示させる
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
   });
 });
